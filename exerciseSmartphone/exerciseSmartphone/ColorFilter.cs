66fe7eafdf53a2d0725ae439d0716e33c0651f87
Lorenzo Antonelli <darthtunelos@hotmail.it>
2019-04-04 18:03:31 +0200

    test esercizio smartphone

---
+++ exerciseSmartphone/exerciseSmartphone/ColorFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciseSmartphone
+{
+    class ColorFilter : IFilter<string>
+    {
+        public bool Filter(IList<Smartphone> list, string input)
+        {
+           var res = list.Where(w => w.Color == input).ToList();
+            if (res != null && res.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Smartphone> FilterSmartphones(IList<Smartphone> list, string input)
+        {
+            return list.Where(w => w.Color == input).ToList();
+        }
+    }
+}
